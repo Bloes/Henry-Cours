b538204b0d7e6125c653eb85a4148470492831ab
bloes007 <bloes@outlook.es>
2023-01-08 13:12:24 -0500

    Repositorio JS

---
+++ Operadores.js
@@ -0,0 +1,10 @@
+console.log(3+5);
+console.log(14-7);
+console.log(3/5);
+console.log(3*3);
+
+console.log(35%5);
+console.log(3**5);
+
+console.log(3+5*2-8);
+console.log((3+5)*2-8)
---
+++ Variables.js
@@ -0,0 +1,16 @@
+var cajon1='Budanda verde';
+//String 
+var bootcamp='Henry';
+//Números
+var numeros=5;
+//Booleanos
+var verdadero=true;
+//Underfined
+var cajon1;
+console.log(cajon1);
+//Null
+var sinNada=null;
+
+
+
+
