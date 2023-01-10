1e460d1966769aca68925f6fb97bf2d8b798db07
bloes007 <bloes@outlook.es>
2023-01-10 11:42:20 -0500

    Repositorios

---
+++ Funciones.js
@@ -0,0 +1,23 @@
+function sumatres(x){
+    console.log(x+3);
+};
+sumatres(5)
+//*--------------------------//*
+//PRIMERO
+function sumaTres(x){
+    return x+3
+};
+//SEGUNDO
+var sumaTres = function(x){
+    return x+3
+};
+//TERCERO
+var sumaTres = (x) =>{
+    return x+3
+};
+//*----------------------------------//*
+
+function cuidadoconlaconsalelog(nombre){
+    console.log(nombre);
+    return nombre;
+}
---
+++ OperadoresDeComparacion.js
@@ -0,0 +1,12 @@
+console.log(4<7);
+console.log(4>7);
+console.log(4==4);
+
+console.log(5===5);
+console.log(5==='5');
+console.log(5=='5');
+
+var a = 1;
+var b = 2;
+var c = a = b;
+console.log(c);
