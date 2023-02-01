3cce3523112fa05f9779435defede17e6e8045cc
bloes007 <bloes@outlook.es>
2023-02-01 13:05:26 -0500

    Act

--- array1.js
+++ array1.js
@@ -27,3 +27,12 @@ while (arr.length < 10){
     arr.push('BOOM!');
 }
 console.log(arr);
+//*-------------------------------------------*/
+arrayChar = ['a', 'b', 'c', 'd', 'e', 'f'];
+arrayString  = ["Hola", "mundo"];
+arrayNumeros = [1, 2, 3, 4, 5];
+arrayNumerosF = [1.1,2.2];
+
+arrayInvalido = ['a',5,-1,"texto",1.1];    //Los array son de un tipo de lista 
+
+arrayArray = [[1,2,3],[4,5,6],[7,8,9]];
